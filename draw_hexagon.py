2eb79e356df95aebfc28ba84cc51aaf9d3bd2d4a
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-06-11 10:56:27 -0400

    Declare and use draw_hex function

--- draw_hexagon.py
+++ draw_hexagon.py
@@ -1,4 +1,8 @@
-# I am following a Pygame tutorial from Coding With Russ
+
+def draw_hex(screen, x, y, color):
+    hexagon = [(x, y), (x+40, y), (x+60, y+35), (x+40, y+70), (x, y+70), (x-20, y+35)]
+    pygame.draw.polygon(screen, color, hexagon)
+
 
 import pygame
 
@@ -12,8 +16,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Draw Hexagon")
 
 # Create hexagons
-hexagon = [(20, 0), (60, 0), (80, 35), (60, 70), (20, 70), (0, 35)]
-hexagon2 = [(80, 35), (120, 35), (140, 70), (120, 105), (80, 105), (60, 70)]
+# __ hexagon = [(20, 0), (60, 0), (80, 35), (60, 70), (20, 70), (0, 35)]
+# __ hexagon2 = [(80, 35), (120, 35), (140, 70), (120, 105), (80, 105), (60, 70)]
 
 
 run = True
@@ -22,8 +26,11 @@ while run:
     screen.fill((0, 0, 0))
 
     # Draw hexagons
-    pygame.draw.polygon(screen, (5, 5, 180), hexagon)
-    pygame.draw.polygon(screen, (5, 180, 5), hexagon2)
+    draw_hex(screen, 20, 0, (5, 5, 180))
+    draw_hex(screen, 80, 35, (5, 180, 5))
+    draw_hex(screen, 140, 0, (255, 0, 0))
+    # __ pygame.draw.polygon(screen, (5, 5, 180), hexagon)
+    # __ pygame.draw.polygon(screen, (5, 180, 5), hexagon2)
 
     # Event handler (closing window)
     for event in pygame.event.get():
