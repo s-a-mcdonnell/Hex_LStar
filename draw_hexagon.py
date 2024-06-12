1db0a047d2f80291cc4881b75a724be04ef59c27
amklinglerr <amklinglerr@gmail.com>
2024-06-12 10:31:37 -0400

    made function to check if the neighbors are walls

--- draw_hexagon.py
+++ draw_hexagon.py
@@ -90,7 +90,48 @@ class Hex:
                hex_movable[5] = 1
 
         return hex_movable
+   
 
+   def check_walls(self):
+        hex_walls = [0, 0, 0, 0, 0, 0]
+
+        # check upper hex (pos 0)
+        if self.list_index - 1 > 0:
+           hexToCheck = hex_matrix[self.matrix_index][self.list_index - 1]
+           if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[0] = 1
+
+         # check northeast hex (pos 1)
+        if (self.matrix_index + 1 < len(hex_matrix)) and (self.list_index - 1 > 0):
+           hexToCheck = hex_matrix[self.matrix_index + 1][self.list_index - 1]
+           if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[1] = 1
+
+        # check southeast hex (pos 2)
+        if self.matrix_index + 1 < len(hex_matrix):
+            hexToCheck = hex_matrix[self.matrix_index + 1][self.list_index]
+            if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[2] = 1
+
+        # check down hex (pos 3)
+        if self.list_index + 1 < len(hex_list):
+            hexToCheck = hex_matrix[self.matrix_index][self.list_index + 1]
+            if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[3] = 1
+
+        # check southwest hex (pos 4)
+        if (self.matrix_index - 1 > 0) and (self.list_index + 1 < len(hex_list)):
+            hexToCheck = hex_matrix[self.matrix_index - 1][self.list_index + 1]
+            if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[4] = 1
+
+         # check northwest hex (pos 5)
+        if self.matrix_index - 1 > 0:
+            hexToCheck = hex_matrix[self.matrix_index - 1][self.list_index]
+            if (hexToCheck.movable == False) and (hexToCheck.occupied == True):
+               hex_walls[5] = 1
+
+        return hex_walls
 
 
     #update self hexagon
@@ -102,6 +143,7 @@ class Hex:
         future.occupied = False
 
         neighbors_movable = self.check_movables()
+        neighbors_wall = self.check_walls()
 
         if(hex_matrix[self.matrix_index][self.list_index].movable == False):
             future.movable = False
