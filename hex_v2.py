51dc661d27b3a6f32262b2f6029fa0d29b5cab53
amklinglerr <amklinglerr@gmail.com>
2024-06-20 10:50:53 -0400

    basic framework for new idea

---
+++ hex_v2.py
@@ -0,0 +1,27 @@
+import time
+import copy
+import os
+import pygame
+
+'''
+Process of the game:
+0. Iterate over Idents
+1. a: If an ident is in a head on collision with another ident, it flips in place
+   b: Else, move the ident forward into the next hex
+2. Resolve collisions. For hexes that contain multiple idents...
+   a: If it has an ident of opposite direction in that hex, bounce off/reverse direction
+   b: Else, take the average of all other idents EXCEPT SELF, but break ties by using the opposite ident of self
+'''
+
+# hex class is now just for graphics/displaying the board/storing idents
+class Hex:
+    pass
+
+# for storing information about a particular moving hex
+class Ident:
+    pass
+
+# for setting initial state of the world / having a student interact
+# while loop for running game goes in World
+class World:
+    pass
