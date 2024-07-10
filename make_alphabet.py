9166473a63910912a184657dcad901be882c10db
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-10 15:09:51 -0400

    Add method to write all 12-bit strings to alphabet.txt

---
+++ make_alphabet.py
@@ -0,0 +1,28 @@
+def write_alphabet():
+    # Writing to a text file according to this tutorial: https://www.w3schools.com/python/python_file_write.asp
+
+    # Overwrite text in alphabet file with the empty string
+    # TODO: Deal with case where file doesn't exist
+    f = open("alphabet.txt", "w")
+    f.write("")
+    f.close()
+
+    # Re-open file to append text
+    f = open("alphabet.txt", "a")
+    four_bits = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"]
+
+    # Write all combinations of three four-bit strings to the file
+    for i in range(16):
+        for j in range(16):
+            for k in range(16):
+                next_letter = four_bits[i] + four_bits[j] + four_bits[k]
+
+                f.write(next_letter + "\n")
+    
+    f.close()
+
+    '''#open and read the file after the overwriting:
+    f = open("demofile3.txt", "r")
+    print(f.read())'''
+
+write_alphabet()
