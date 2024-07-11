1561e79a082c885b76bffee091ee104526841ff9
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-11 13:47:01 -0400

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,9 @@
+from movement_teacher import Movement_Teacher
+from direction_teacher import Direction_Teacher
+
+mover = Movement_Teacher()
+director = Direction_Teacher()
+
+# Learn movement teacher using L*
+
+# Learn direction teacher using L*
