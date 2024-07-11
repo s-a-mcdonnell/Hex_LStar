295d352ffc1709b72b0b487bb73cf7e9580c5b23
amklinglerr <amklinglerr@gmail.com>
2024-07-11 14:10:54 -0400

    updates to teacher subclasses

--- movement_teacher.py
+++ movement_teacher.py
@@ -10,72 +10,10 @@ class Movement_Teacher(Teacher):
 
     # TODO: Create option not to read agent file?
     def __create_world(self, s):
-        new_world = World(read_file=False)
-            
-        # Parse string into world
-        # TODO: the forcibly converting it into an integer could cause problems later. Note to self, be careful.
-        for i in range(int((len(s))/3)):
-            # splice the three character string into three one-character chunks
-            property = int(s[i*3], 16)
-            mi = int(input[i*3 + 1], 16)
-            li = int(input[i*3 + 2], 16)
-
-            new_ident = Ident(mi, li, new_world)
-
-            new_world.hex_matrix[mi][li].idents.append(new_ident)
-            
-            # The first char in ever "letter" (3-char string) form the property
-            # The properties are wall (0), stationary non-agent (1), moving agent (in directions 0 through 5, 1 through 7),
-            # stationary agent (8), moving agent (in directions 0 through 5, 9 through e), and goal (f)
-
-            # 0 => wall
-            if property == 0:
-                new_ident.state = -2
-                new_world.wall_list.append(new_ident)
-
-            # If not a wall, it goes on the ident list
-            else:
-                new_world.ident_list.append(new_ident)
-
-            # 1 => stationary (non-agent)
-            if property == 1:
-                new_ident.state = -1
-            
-            # 2 => direction 0 (non-agent)
-            # 3 => direction 1 (non-agent)
-            # 4 => direction 2 (non-agent)
-            # 5 => direction 3 (non-agent)
-            # 6 => direction 4 (non-agent)
-            # 7 => direction 5 (non-agent)
-            elif property >= 2 and property <= 7:
-                new_ident.state = property - 2
-            
-            # 8 => stationary (agent)
-            if property == 8:
-                new_ident.state = -1
-                new_world.agents.append(new_ident)
-            
-            # 9 => direction 0 (agent)
-            # 10 => direction 1 (agent)
-            # 11 => direction 2 (agent)
-            # 12 => direction 3 (agent)
-            # 13 => direction 4 (agent)
-            # 14 => direction 5 (agent)
-            elif property >= 9 and property <= 14:
-                new_ident.state = property - 9
-                new_world.agents.append(new_ident)
-
-            # 15 => goal (stationary)
-            elif property == 15:
-                new_ident.state = -1
-                # Mark as goal
-                new_ident.property = "goal"
-            
-            # Save the first ident described in the string as my_agent
-            if i == 0:
-                self.my_agent = new_ident
-        
-        self.world = new_world    
+        # TODO: does super() work when you're returning something??? hopefully it still makes self.world...
+        # TODO: test to see if this works properly
+        self.world = World(read_file = False)
+        super().__create_world(s)   
 
 
     # membership query
