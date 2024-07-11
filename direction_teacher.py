5007141903b063e7ed50a24953f13c5744654136
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-11 13:35:16 -0400

    Create subclasses of teacher

---
+++ direction_teacher.py
@@ -0,0 +1,7 @@
+# The second teacher (for an agent that is turning, is it turning clockwise or counterclockwise?)
+
+from teacher import Teacher
+
+class Direction_Teacher(Teacher):
+    def __init__(self):
+        pass
---
+++ movement_teacher.py
@@ -0,0 +1,7 @@
+# The first teacher (is the agent turning itself?)
+
+from teacher import Teacher
+
+class Movement_Teacher(Teacher):
+    def __init__(self):
+        pass
