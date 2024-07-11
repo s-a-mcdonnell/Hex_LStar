b87b8e8cca4925092d2b773d25fdc7d4db7e998d
amklinglerr <amklinglerr@gmail.com>
2024-07-11 14:18:09 -0400

    edits to teacher.py

--- teacher.py
+++ teacher.py
@@ -204,42 +204,15 @@ class Teacher:
     def member(self, s : str, dfa: list[list[int]] = None, alpha = None):
         # print("membership query called")
 
+        if not dfa:
+            dfa = self.m
+        
         if not alpha:
             alpha = self.alphabet
 
-        # If passed a matrix, use it as the dfa and return boolen indicating final state action
-        if dfa:        
-            # Return the int boolean indicating if the final state is an accept or reject state
-            final_state : list[int] = Teacher.final_state(s, dfa, alpha)
-            return bool(final_state[0])
-        
-        # If not passed a matrix, return an answer as if the agent's decision-making process were a DFA
-        else:
-            # Always reject the empty string (arbitrary decision)
-            if s == "":
-                return False
-            
-            self.__create_world(s)
-            assert self.world
-            assert self.my_agent
-            
-            original_agent_state = self.my_agent.state
-
-            # TODO: Run one loop of updating the world and check was the agent's state is
-            # TODO: How to know what part of the agent instructions the world should be looking at? (potentially big issue, since we've created a world from scratch)
-            self.world.update()
-
-            new_state = Ident.find_next_move(self.my_agent)
-            # TODO: the world updating effects the agent though????
-
-            # TODO: Return a boolean corresponding to the agent's state
-            # TODO: Actually we want to just report the agent's action, not how it might have been affected by the physics rules
-            if new_state == 0:
-                return False
-            # false on first DFA => we are not manually changing the agent's direction (ie -> instruction 0)
-            # true on first DFA => we are changing the agent's direction via the agent (ie -> instruction -1 or 1)
-            else:
-                return True
+        # Return the int boolean indicating if the final state is an accept or reject state
+        final_state : list[int] = Teacher.final_state(s, dfa, alpha)
+        return bool(final_state[0])
     
     ##########################################################################################################
 
