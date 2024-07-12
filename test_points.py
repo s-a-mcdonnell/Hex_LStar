7343dbcb5e4e368210cf49c05f6190d7570a6d09
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-12 14:20:46 -0400

    Use randomly-generated test points

--- test_points.py
+++ test_points.py
@@ -39,7 +39,11 @@ print(f"Agent move from string 947f68: {__get_move("947f68")}")
 print(f"Agent move from string c67f48357: {__get_move("c67f48357")}")
 print(f"Agent move from string ba6f48857: {__get_move("ba6f48857")}")
 
-for i in range(1, len(sys.argv)):
+'''for i in range(1, len(sys.argv)):
     assert len(sys.argv[i])%3 == 0
     assert len(sys.argv[i]) >= 6
-    print(f"Agent move from string {sys.argv[i]}: {__get_move(sys.argv[i])}")    
+    print(f"Agent move from string {sys.argv[i]}: {__get_move(sys.argv[i])}")'''    
+
+for i in range(20):
+    new_string = Teacher.generate_string()
+    print(f"Agent move from string {new_string}: {__get_move(new_string)}")
